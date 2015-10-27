b1304d56f68c78d0113c499999fb732d284f0a7b
Perry van den Hondel <perry.vandenhondel@gmail.com>
2015-10-27 15:05:35 +0100

    Added extra table fields: whenCreated

--- update_tables.pas
+++ update_tables.pas
@@ -40,6 +40,7 @@ const
 	FLD_ATV_SAM = 			'atv_sam';
 	FLD_ATV_MAIL = 			'atv_mail';
 	FLD_ATV_SORT = 			'atv_sort';
+	FLD_ATV_CREATED = 		'atv_created';
 	FLD_ATV_IS_ACTIVE = 	'atv_is_active';
 	FLD_ATV_RLU = 			'atv_rlu';
 
@@ -59,9 +60,13 @@ begin
 end; // of procedure DeleteObsoleteRecords
 	
 	
-procedure RecordAddAccount(dn: string; fname: string; lname: string; upn: string; sam: string; mail: string);
+procedure RecordAddAccount(dn: string; fname: string; lname: string; upn: string; sam: string; mail: string; created: string);
 //
 //	Add a new record to the table when it does not exist yet, key = dn.
+//	
+//		dn:		Distinguished Name of the object
+//		fname:	First name
+//		lname:	Last name
 //
 var
 	qs: string;
@@ -99,7 +104,7 @@ begin
 		qi := qi + FLD_ATV_UPN + '=' + FixStr(upn) + ',';
 		qi := qi + FLD_ATV_SAM + '=' + FixStr(sam) + ',';
 		qi := qi + FLD_ATV_MAIL + '=' + FixStr(mail) + ',';
-		
+		qi := qi + FLD_ATV_CREATED + '=' + FixStr(created) + ',';
 		qi := qi + FLD_ATV_RLU + '=' + EncloseSingleQuote(DateTimeToStr(updateDateTime)) + ';';
 		//WriteLn(qi);
 		RunQuery(qi);
@@ -119,6 +124,7 @@ begin
 		qu := qu + FLD_ATV_UPN + '=' + FixStr(upn) + ',';
 		qu := qu + FLD_ATV_SAM + '=' + FixStr(sam) + ',';
 		qu := qu + FLD_ATV_MAIL + '=' + FixStr(mail) + ',';
+		qu := qu + FLD_ATV_CREATED + '=' + FixStr(created) + ',';
 		qu := qu + FLD_ATV_RLU + '=' + EncloseSingleQuote(DateTimeToStr(updateDateTime)) + ' ';
 		qu := qu + 'WHERE ' + FLD_ATV_ID + '=' + IntToStr(id) + ';';
 		//WriteLn(qu);
@@ -177,8 +183,10 @@ begin
 	c := c + '-b "' + ou + ',' + rootDse + '" ';
 	//c := c + '-f "sAMAccountName=*_*" ';
 	c := c + '-f "(&(objectCategory=person)(objectClass=user))" ';
-	c := c + 'sAMAccountName givenName sn userPrincipalName mail ';
-	c := c + '-csv -nocsvq -csvdelim ;' ;
+	c := c + 'sAMAccountName givenName sn userPrincipalName mail whenCreated ';
+	c := c + '-csv -nocsvq -csvdelim ; ';
+	// Convert the whenCreated AD datetime to readable format.
+	c := c + '-tdcgt -tdcfmt "%YYYY%-%MM%-%DD% %HH%:%mm%:%ss%"';
 	c := c + '>' + f;
 	WriteLn(c);
 	
@@ -207,7 +215,7 @@ begin
 		if IsValidAdminAccount(dn) = true then
 		begin
 			//dn;sAMAccountName;givenName;sn;userPrincipalName
-			RecordAddAccount(dn, csv.GetValue('givenName'), csv.GetValue('sn'), csv.GetValue('userPrincipalName'), csv.GetValue('sAMAccountName'), csv.GetValue('mail'));
+			RecordAddAccount(dn, csv.GetValue('givenName'), csv.GetValue('sn'), csv.GetValue('userPrincipalName'), csv.GetValue('sAMAccountName'), csv.GetValue('mail'), csv.GetValue('whenCreated'));
 			//WriteLn('dn=', dn);
 		end; // of if
     until csv.GetEof();
